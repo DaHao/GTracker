b820747bb0e51ed14ce8e2eb371ec8b75875ab8e
Hao <jiash561@gmail.com>
2022-08-19 16:54:09 +0800

    updated

--- report.js
+++ report.js
@@ -1,63 +1,135 @@
+require('dotenv').config();
+
 const axios = require('axios');
+const asyncjs = require('async');
 
-async function callApi(options) {
-  const result = await axios.request(options)
-    .then(res => res)
-    .catch(err => err);
+const PORTAL_ID = 14003804;
+const TRACKING_TYPE = ['plan', 'coding', 'fix', 'review'];
 
-  return result;
+function isTrackingNote(content) {
+  return TRACKING_TYPE.includes(content?.trim());
 }
 
-const options = {
-  method: 'GET',
-  baseURL: 'https://gitlab.com/api/v4',
-  url: '/issues',
-  params: {
-    private_token: 'glpat-MnTX6mgpLwypnVd3jbsx',
-    milestone: 'GMN v1.8.2',
-  },
-};
-
-const noteOptions = {
-  method: 'GET',
-  baseURL: 'https://gitlab.com/api/v4',
-  url: '/projects/14003804/issues/642/notes',
-  params: {
-    private_token: 'glpat-MnTX6mgpLwypnVd3jbsx',
-    milestone: 'GMN v1.8.2',
-  },
-};
-
-const options2 = {
-  method: 'GET',
-  baseURL: 'https://gitlab.com/api/v4',
-  url: '/projects/14003804/issues/651/time_stats',
-  params: {
-    private_token: 'glpat-MnTX6mgpLwypnVd3jbsx',
-    milestone: 'GMN v1.8.2',
-  },
+function callApi(options) {
+  return axios.request(options);
 }
 
-function getIssueContent(entry) {
-  const {
-    id, title,
-    milestone: { id: mid, title: mTitle },
-    web_url, time_stats, _links, references,
-  } = entry;
-
-  return { id, title, time_stats, _links, references, milestone: { id: mid, title: mTitle }, web_url };
+function getIssueApiOptions(projectId, query) {
+  return {
+    baseURL: 'https://gitlab.com/api/v4/',
+    url: `projects/${projectId}/issues`,
+    method: 'GET',
+    params: {
+      private_token: process.env.TOKEN,
+      scope: 'all',
+      order_by: 'updated_at',
+      ...query,
+    },
+  };
 }
 
-function getNoteContent(entry) {
-  const { id, body, author, created_at, updated_at } = entry;
-  return { id, body, author, created_at, updated_at };
+function getIssueContent(issue) {
+  const { title, _links, updated_at } = issue;
+  // return { title, links: _links, updated_at };
+  return issue;
+}
+// curl -XGET https://gitlab.com/api/v4/projects/14003804/issues/670/notes?private_token\=glpat-MnTX6mgpLwypnVd3jbsx\&order_by\=updated_at
+
+function getNoteApiOptions(url) {
+  return {
+    url,
+    method: 'GET',
+    params: {
+      private_token: process.env.TOKEN,
+      sort: 'asc',
+    },
+  };
+}
+
+function convertTime(token) {
+  const pattern = /(\d+)([dhm]+)/;
+  let [,time, unit] = token.match(pattern);
+
+  if (unit === 'd') time = Number(time) * 8;
+  if (unit === 'm') time = Number(time) / 60;
+
+  return Number(time);
+}
+
+function getSpentTime(content) {
+  const pattern = /added (.*) of time spent/;
+
+  let [, spentTime] = content?.match(pattern);
+
+  return spentTime.split(' ')?.reduce((accu, token) => {
+    const time = convertTime(token);
+    return accu + Number(time);
+  }, 0);
+}
+
+function getNotesReport(notes) {
+  const report = [];
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i];
+    if (!isTrackingNote(note?.body)) continue;
+
+    const spentNote = notes[i + 1];
+    if (!spentNote || spentNote.author?.name !== note.author?.name) continue;
+
+    const spentTime = getSpentTime(spentNote.body);
+
+    report.push({
+      authorName: note.author?.name,
+      summary: note.body,
+      time: spentTime,
+    });
+  }
+
+  return report;
+}
+
+async function getIssueReport(issue) {
+  const { id, iid, title, _links, time_stats } = issue;
+  const noteUrl = _links?.notes;
+
+  if (!noteUrl) return [];
+
+  const notes = await callApi(getNoteApiOptions(noteUrl))
+    .then(resp => resp?.data)
+    .catch(error => console.log(error));
+
+  const report = getNotesReport(notes);
+
+  return {
+    iid,
+    title,
+    report,
+    time_estimate: time_stats?.human_time_estimate,
+  };
 }
 
 async function main() {
-  const resp = await callApi(noteOptions);
-  const result = resp?.data?.map(getNoteContent);
-  console.log(result);
+  const page = 1;
+  const issueOpts = getIssueApiOptions(PORTAL_ID, {
+    updated_after: new Date('2022-08-14T00:00:00Z'),
+    page,
+    per_page: 100,
+  });
+
+  const issues = await callApi(issueOpts)
+    .then(resp => resp.data)
+    .catch(error => console.log(error));
+
+  const report = await asyncjs.mapSeries(issues, getIssueReport)
+    .catch(error => console.log(error));
+  
+  // const testIssue = issues[1];
+  // const result = await getIssueReport(testIssue);
+  console.log(report);
+
+  // report.forEach(row => console.log(row));
+  // console.log(resp?.headers);
+  // console.log(`Total: ${result?.length}`);
 }
-// https://gitlab.com/geminiopencloud/engineering/portal/xportal/-/issues/659#
 
 main();
